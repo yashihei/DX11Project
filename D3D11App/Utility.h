d5e23d25fe047de69d60b83293846bde2e897226
yashihei <yashihei@gmail.com>
2017-04-27 10:10:04 +0900

    Improve string util functions.

--- D3D11App/Utility.h
+++ D3D11App/Utility.h
@@ -2,7 +2,9 @@
 
 #include <WinBase.h>
 #include <string>
-#include <codecvt>
+#include <WinNls.h>
+#include <vector>
+#include <stdexcept>
 
 template <typename Type> inline void OutputDebugValue(Type& value) {
 #if defined(DEBUG) || defined(_DEBUG)
@@ -28,16 +30,28 @@ template <typename Container, typename Pred> inline void Erase_if(Container& c, 
 	c.erase(std::remove_if(c.begin(), c.end(), pred), c.end());
 }
 
-inline std::wstring s2ws(const std::string& str)
-{
-    using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
-    return converterX.from_bytes(str);
+//from:http://nekko1119.hatenablog.com/entry/2017/01/02/054629
+inline std::wstring s2ws(const std::string& src) {
+	auto const dest_size = ::MultiByteToWideChar(CP_ACP, 0U, src.data(), -1, nullptr, 0U);
+    std::vector<wchar_t> dest(dest_size, L'\0');
+    if (::MultiByteToWideChar(CP_ACP, 0U, src.data(), -1, dest.data(), dest.size()) == 0) {
+        throw std::system_error{static_cast<int>(::GetLastError()), std::system_category()};
+    }
+    return std::wstring(dest.data());
 }
 
-inline std::string ws2s(const std::wstring& wstr)
-{
-    using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
-    return converterX.to_bytes(wstr);
+inline std::string ws2s(const std::wstring& src) {
+    auto const dest_size = ::WideCharToMultiByte(CP_ACP, 0U, src.data(), -1, nullptr, 0, nullptr, nullptr);
+    std::vector<char> dest(dest_size, '\0');
+    if (::WideCharToMultiByte(CP_ACP, 0U, src.data(), -1, dest.data(), dest.size(), nullptr, nullptr) == 0) {
+        throw std::system_error{static_cast<int>(::GetLastError()), std::system_category()};
+    }
+    return std::string(dest.data());
+}
+
+inline std::string GetDirPath(const std::string& filePath) {
+	auto const dest_size = filePath.size();
+	std::vector<char> dest(dest_size, '\0');
+	_splitpath_s(filePath.c_str(), NULL, 0, dest.data(), dest_size, NULL, 0, NULL, 0);
+	return std::string(dest.data());
 }
