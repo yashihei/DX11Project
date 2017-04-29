342a1e34b43794ca3cc50beeaff0936172f7af36
yashihei <yashihei@gmail.com>
2017-04-29 14:01:55 +0900

    Update camera

--- D3D11App/Camera.h
+++ D3D11App/Camera.h
@@ -6,10 +6,17 @@ using namespace DirectX::SimpleMath;
 class Camera
 {
 public:
-	Camera() {}
-	void setPos(Vector3 pos) { m_pos = pos; }
-	void setRot(Vector3 rot) { m_rot = rot; }
+	Camera(const Vector3& pos, const Vector3& lookAt, float aspect) :
+		m_pos(pos), m_lookAt(lookAt) {
+		m_proj = DirectX::XMMatrixPerspectiveFovLH(3.141592f / 4.0f, aspect, 0.1f, 1000.0f);
+	}
+	Matrix getViewMat() const {
+		return DirectX::XMMatrixLookAtLH(m_pos, m_lookAt, { 0, 1, 0 });
+	}
+	Matrix getProjMat() const {
+		return m_proj;
+	}
 private:
-	Vector3 m_pos, m_rot;
-	Matrix m_viewMat, m_projMat;
+	Vector3 m_pos, m_lookAt;
+	Matrix m_proj;
 };
