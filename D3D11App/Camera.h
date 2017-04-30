188b6e4ee9be562b0c057a9bd279565fc06249a2
yashihei <yashihei@gmail.com>
2017-04-30 23:25:31 +0900

    Improve Camera class.

--- D3D11App/Camera.h
+++ D3D11App/Camera.h
@@ -6,18 +6,18 @@ using namespace DirectX::SimpleMath;
 class Camera
 {
 public:
-	Camera(const Vector3& _pos, const Vector3& _lookAt, float aspect) :
-		pos(_pos), lookAt(_lookAt) {
+	Camera(const Vector3& _pos, const Vector3& _lookAt, const Vector3& up, float aspect) :
+		pos(_pos), lookAt(_lookAt), up(up) {
 		m_proj = DirectX::XMMatrixPerspectiveFovLH(3.141592f / 4.0f, aspect, 0.1f, 1000.0f);
 	}
 	Matrix getViewMat() const {
-		return DirectX::XMMatrixLookAtLH(pos, lookAt, { 0, 1, 0 });
+		return DirectX::XMMatrixLookAtLH(pos, lookAt, up);
 	}
 	Matrix getProjMat() const {
 		return m_proj;
 	}
 public:
-	Vector3 pos, lookAt;
+	Vector3 pos, lookAt, up;
 private:
 	Matrix m_proj;
 };
