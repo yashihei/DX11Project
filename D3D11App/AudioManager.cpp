a809aaa9f6fc51b4a594d37d25321bd40c56ea1c
yashihei <yashihei@gmail.com>
2017-04-30 19:17:56 +0900

    Improve AudioManager
    + loop, pause, stop

--- D3D11App/AudioManager.cpp
+++ D3D11App/AudioManager.cpp
@@ -20,14 +20,45 @@ void AudioManager::update()
 	}
 }
 
-void AudioManager::load(const std::string & filePath, const std::string & alias)
+void AudioManager::load(const std::string& filePath, const std::string& alias)
 {
-	auto soundEffect = std::make_unique<DirectX::SoundEffect>(m_audioEngine.get(), s2ws(filePath).c_str());
-	m_soundEffects[alias] = std::move(soundEffect);
+	m_soundEffects[alias] = std::make_unique<DirectX::SoundEffect>(m_audioEngine.get(), s2ws(filePath).c_str());
+	m_soundEffectsInstance[alias] = m_soundEffects[alias]->CreateInstance();
 }
 
-void AudioManager::play(const std::string& alias, float volume, float pitch)
+void AudioManager::playMulti(const std::string& alias, float volume, float pitch)
 {
 	assert(m_soundEffects.count(alias) != 0);
 	m_soundEffects[alias]->Play(volume, pitch, 0);
+}
+
+void AudioManager::play(const std::string& alias, bool loop, float volume, float pitch)
+{
+	assert(m_soundEffects.count(alias) != 0);
+
+	//ポーズ中なら再開する
+	if (m_soundEffectsInstance[alias]->GetState() == DirectX::SoundState::PAUSED) {
+		m_soundEffectsInstance[alias]->Resume();
+		return;
+	}
+
+	m_soundEffectsInstance[alias]->SetVolume(volume);
+	m_soundEffectsInstance[alias]->SetPitch(pitch);
+	m_soundEffectsInstance[alias]->Play(loop);
+}
+
+void AudioManager::pause(const std::string& alias)
+{
+	assert(m_soundEffects.count(alias) != 0);
+	if (m_soundEffectsInstance[alias]->GetState() == DirectX::SoundState::PLAYING) {
+		m_soundEffectsInstance[alias]->Pause();
+	}
+}
+
+void AudioManager::stop(const std::string& alias)
+{
+	assert(m_soundEffects.count(alias) != 0);
+	if (m_soundEffectsInstance[alias]->GetState() == DirectX::SoundState::PLAYING) {
+		m_soundEffectsInstance[alias]->Stop();
+	}
 }
--- D3D11App/AudioManager.h
+++ D3D11App/AudioManager.h
@@ -15,8 +15,14 @@ public:
 	AudioManager();
 	void update();
 	void load(const std::string& filePath, const std::string& alias);
-	void play(const std::string& alias, float volume = 1.0f, float pitch = 0.0f);
+	void playMulti(const std::string& alias, float volume = 1.0f, float pitch = 0.0f);
+	void play(const std::string& alias, bool loop = false, float volume = 1.0f, float pitch = 0.0f);
+	void pause(const std::string& alias);
+	void stop(const std::string& alias);
+	//void fadein(const std::string& alias);
+	//void fadeout(const std::string& alias);
 private:
 	std::unique_ptr<DirectX::AudioEngine> m_audioEngine;
 	std::unordered_map<std::string, std::unique_ptr<DirectX::SoundEffect>> m_soundEffects;
+	std::unordered_map<std::string, std::unique_ptr<DirectX::SoundEffectInstance>> m_soundEffectsInstance;
 };
