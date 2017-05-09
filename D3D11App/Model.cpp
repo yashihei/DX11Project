a28470ac13cec32da51237c9b63168b086940bd7
yashihei <yashihei@gmail.com>
2017-05-10 06:44:50 +0900

    Update createFromObj()

--- D3D11App/Model.cpp
+++ D3D11App/Model.cpp
@@ -5,6 +5,8 @@
 #include "ShaderRV.h"
 #include "Utility.h"
 #include "BasicEffect.h"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "ObjLoader/tiny_obj_loader.h"
 
 Model::Model(ComPtr<ID3D11Device> device, ComPtr<ID3D11DeviceContext> deviceContext) :
 	m_device(device), m_deviceContext(deviceContext)
@@ -99,6 +101,46 @@ void Model::createFromPmx(const std::string& filePath)
 
 void Model::createFromObj(const std::string & filePath)
 {
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+
+	std::string err;
+	bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, filePath.c_str(), GetDirPath(filePath).c_str());
+	if (!err.empty()) {
+		throw std::runtime_error(filePath + " Load Failed.");
+	}
+
+	//// Loop over shapes
+	for (size_t s = 0; s < shapes.size(); s++) {
+		size_t index_offset = 0;
+		// Loop over faces(polygon)
+		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+			int fv = shapes[s].mesh.num_face_vertices[f];
+			// Loop over vertices in the face.
+			for (size_t v = 0; v < fv; v++) {
+				// access to vertex
+				tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+
+				tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
+				tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
+				tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
+
+				if (attrib.normals.size() == 0) break;
+				tinyobj::real_t nx = attrib.normals[3 * idx.normal_index + 0];
+				tinyobj::real_t ny = attrib.normals[3 * idx.normal_index + 1];
+				tinyobj::real_t nz = attrib.normals[3 * idx.normal_index + 2];
+
+				if (attrib.texcoords.size() == 0) break;
+				tinyobj::real_t tx = attrib.texcoords[2 * idx.texcoord_index + 0];
+				tinyobj::real_t ty = attrib.texcoords[2 * idx.texcoord_index + 1];
+			}
+			index_offset += fv;
+
+			// per-face material
+			shapes[s].mesh.material_ids[f];
+		}
+	}
 }
 
 void Model::createVertexBuffer()
