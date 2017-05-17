bb5576612a94e336a9bb55b879755a04e30ab34f
yashihei <yashihei@gmail.com>
2017-05-17 14:51:30 +0900

    Mod camera far

--- D3D11App/Camera.h
+++ D3D11App/Camera.h
@@ -8,7 +8,7 @@ class Camera
 public:
 	Camera(const Vector3& _pos, const Vector3& _lookAt, const Vector3& up, float aspect) :
 		pos(_pos), lookAt(_lookAt), up(up) {
-		m_proj = DirectX::XMMatrixPerspectiveFovLH(3.141592f / 4.0f, aspect, 0.1f, 1000.0f);
+		m_proj = DirectX::XMMatrixPerspectiveFovLH(3.141592f / 4.0f, aspect, 0.1f, 10000.0f);
 	}
 	Matrix getViewMat() const {
 		return DirectX::XMMatrixLookAtLH(pos, lookAt, up);
