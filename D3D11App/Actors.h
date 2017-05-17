605a3eff371e5480b6b7233154961b85a8f49221
yashihei <yashihei@gmail.com>
2017-05-17 12:56:04 +0900

    Update PlayerMove

--- D3D11App/Actors.h
+++ D3D11App/Actors.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <deque>
 #include "Fwd.h"
 #include "Model.h"
 #include "InputManager.h"
@@ -15,17 +16,34 @@ public:
 		m_pos(Vector3::Zero), m_rotation(Vector3::Zero) {
 	}
 	void update() {
-		if (m_inputManager->isPressedLeft())
-			m_rotation.y -= 0.03f;
-		if (m_inputManager->isPressedRight())
-			m_rotation.y += 0.03f;
+		if (m_inputManager->isPressedLeft()) {
+			m_advance.y -= 0.003f;
+			m_advance.z += 0.006f;
+		}
+		if (m_inputManager->isPressedRight()) {
+			m_advance.y += 0.003f;
+			m_advance.z -= 0.006f;
+		}
 		if (m_inputManager->isPressedUp())
-			m_rotation.x += 0.03f;
+			m_advance.x += 0.003f;
 		if (m_inputManager->isPressedDown())
-			m_rotation.x -= 0.03f;
+			m_advance.x -= 0.003f;
 
-		Matrix rot = Matrix::CreateFromYawPitchRoll(m_rotation.y, m_rotation.x, m_rotation.z);
-		m_pos += rot.Backward() / 3.0f;
+		m_advance *= 0.90f;
+		m_rotation += m_advance;
+		m_rotation.z *= 0.95f;
+
+		m_track.push_front(Matrix::CreateFromYawPitchRoll(m_rotation.y, m_rotation.x, 0));
+		if (m_track.size() > 15)
+			m_track.pop_back();
+
+		m_pos += m_track.begin()->Backward();
+
+		const Matrix nowRotMat = *m_track.rbegin();
+		//tps camera
+		m_camera->up = nowRotMat.Up();
+		m_camera->pos = m_pos + nowRotMat.Forward() * 20;
+		m_camera->lookAt = m_pos + nowRotMat.Backward();
 	}
 	void draw() {
 		Matrix scale, rot, trans;
@@ -45,5 +63,6 @@ private:
 	ModelPtr m_model;
 	InputManagerPtr m_inputManager;
 	CameraPtr m_camera;
-	Vector3 m_pos, m_rotation;
+	Vector3 m_pos, m_rotation, m_advance;
+	std::deque<Matrix> m_track;
 };
