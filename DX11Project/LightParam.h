1e9b251d73a1764a3fa164ffc16271b3beb960d6
yashihei <yashihei@gmail.com>
2017-09-14 11:11:06 +0900

    Add LightParam.h

---
+++ DX11Project/LightParam.h
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------
+// File : LightParam.h
+// Copyright (c) 2017 yashihei
+// Licensed under the MIT license
+// -----------------------------------------------
+
+#pragma once
+
+#include "DirectXTK/SimpleMath.h"
+
+using namespace DirectX::SimpleMath;
+
+struct LightParam
+{
+	LightParam() = default;
+
+	LightParam(const Vector4& diffuse, const Vector4& ambient, const Vector3& direction)
+		: diffuse(diffuse),
+		  ambient(ambient),
+		  direction(direction) {}
+
+	Vector4 diffuse;
+	Vector4 ambient;
+	Vector3 direction;
+};
