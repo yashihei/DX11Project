61317c0c30727ae84ab6b3d61d489abf868804e0
yashihei <yashihei@gmail.com>
2017-09-14 10:01:29 +0900

    Ready light

---
+++ DX11Project/Light.h
@@ -0,0 +1,12 @@
+﻿// -----------------------------------------------
+// File : Light.h
+// Copyright (c) 2017 yashihei
+// Licensed under the MIT license
+// -----------------------------------------------
+
+#pragma once
+
+class Light
+{
+public:
+};
