4fce2bd413f284f3505a88e8fa4932d2ade907db
yashihei <yashihei@gmail.com>
2017-09-18 07:35:02 +0900

    Add aliasMath

---
+++ DX11Project/AliasMath.h
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------
+// File : AliasMath.h
+// Copyright (c) 2017 yashihei
+// Licensed under the MIT license
+// -----------------------------------------------
+
+#pragma once
+
+#include <d3d11.h>
+#include "DirectXTK/SimpleMath.h"
+
+using Vector2 = DirectX::SimpleMath::Vector2;
+using Vector3 = DirectX::SimpleMath::Vector3;
+using Vector4 = DirectX::SimpleMath::Vector4;
+using Matrix = DirectX::SimpleMath::Matrix;
+using Quaternion = DirectX::SimpleMath::Quaternion;
+using Plane = DirectX::SimpleMath::Plane;
+using Ray = DirectX::SimpleMath::Ray;
+using Rect = DirectX::SimpleMath::Rectangle;
+using Color = DirectX::SimpleMath::Color;
