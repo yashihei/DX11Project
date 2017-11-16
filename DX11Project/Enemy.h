fdd7bd35fba0e25ac56d6ff99f1f390ea27ac95e
yashihei <yashihei@gmail.com>
2017-11-17 04:51:37 +0900

    Update Enemy

--- DX11Project/Enemy.h
+++ DX11Project/Enemy.h
@@ -7,11 +7,31 @@
 #pragma once
 
 #include "Actor.h"
+#include "Model.h"
 
 class Enemy : public Actor
 {
 public:
-	Enemy()
+	Enemy(ModelPtr model, Vector3 spawnPos) :
+		m_model(model),
+		m_pos(spawnPos), m_rot(Vector3::Zero)
 	{
 	}
+
+	void update() override
+	{
+		const float rad = std::atan2f(0 - m_pos.z, 0 - m_pos.x);
+		m_pos += Vector3(std::cos(rad), 0, std::sin(rad)) * 0.1f;
+		m_rot += Vector3(0, 0.1f, 0.1f);
+	}
+
+	void draw() override
+	{
+		m_model->draw(m_pos, m_rot);
+	}
+private:
+	ModelPtr m_model;
+	Vector3 m_pos, m_rot;
 };
+
+using EnemyManagerPtr = std::shared_ptr<ActorManager<Enemy>>;
