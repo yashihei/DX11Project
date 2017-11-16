d9268c6ae7dbc9b2f8c2e14a6a7610f694108cff
yashihei <yashihei@gmail.com>
2017-11-17 01:23:42 +0900

    Move player

--- DX11Project/Player.h
+++ DX11Project/Player.h
@@ -9,7 +9,6 @@
 #include "Fwd.h"
 #include "Actor.h"
 #include "Model.h"
-#include "Camera.h"
 #include "InputManager.h"
 
 class Player : public Actor
@@ -19,10 +18,34 @@ public:
 		m_inputManager(inputManager),
 		m_model(model),
 		m_pos(Vector3::Zero), m_rot(Vector3::Zero), m_vec(Vector3::Zero)
-	{}
+	{
+	}
 
 	void update() override
 	{
+		Vector2 dir = {
+			static_cast<float>(m_inputManager->isPressedRight() - m_inputManager->isPressedLeft()),
+			static_cast<float>(m_inputManager->isPressedUp() - m_inputManager->isPressedDown())
+		};
+
+		if (dir != Vector2::Zero)
+		{
+			dir.Normalize();
+			dir *= 0.05f;
+			m_vec.x += dir.x;
+			m_vec.z += dir.y;
+		}
+
+		//FIXME: 
+		m_rot.x += m_vec.z * 0.5f;
+		m_rot.z -= m_vec.x * 0.5f;
+
+		m_vec *= 0.85f;
+		m_pos += m_vec;
+
+		//reset
+		if (m_inputManager->isClicledButton1())
+			m_vec = m_pos = m_rot = Vector3::Zero;
 	}
 
 	void draw() override
