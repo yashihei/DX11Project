f3580c8bf401f7a39fba4eae8f485796804ff8de
yashihei <yashihei@gmail.com>
2017-11-22 11:29:07 +0900

    Improve FPSManager

--- DX11Project/FPSManager.h
+++ DX11Project/FPSManager.h
@@ -9,10 +9,12 @@
 #pragma comment(lib, "winmm.lib")
 #include <synchapi.h>
 #include <timeapi.h>
+#include <deque>
+#include <numeric>
 
 class FPSManager {
 public:
-	FPSManager() {
+	FPSManager() : m_nowFPS(0.0) {
 		m_prevTime = timeGetTime();
 	}
 
@@ -25,14 +27,25 @@ public:
 		}
 
 		const DWORD nowTime = timeGetTime();
-		m_nowFPS = 1000 / (nowTime - m_prevTime);
+		const DWORD passTime = nowTime - m_prevTime;
+
+		if (passTime != 0) {
+			m_sample.push_back(1000.0 / passTime);
+		}
+
+		if (m_sample.size() >= 30) {
+			m_nowFPS = std::accumulate(m_sample.begin(), m_sample.end(), 0.0) / m_sample.size();
+			m_sample.clear();
+		}
+
 		m_prevTime = nowTime;
 
 		timeEndPeriod(1);
 	}
 
-	int getFps() const { return m_nowFPS; }
+	double getFps() const { return m_nowFPS; }
 private:
-	int m_nowFPS;
+	double m_nowFPS;
+	std::deque<double> m_sample;
 	DWORD m_prevTime;
 };
