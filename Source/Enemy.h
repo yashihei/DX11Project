b7ac2aad0e5f4cbe4045d3a941cb5973fb52daf0
yashihei <yashihei@gmail.com>
2017-12-04 17:07:30 +0900

    Fix rotate bug

--- Source/Enemy.h
+++ Source/Enemy.h
@@ -15,7 +15,7 @@
 class Enemy : public Actor {
 public:
 	Enemy(App* app, Vector3 spawnPos, Vector3 target) :
-		m_app(app), m_pos(spawnPos), m_rot(Vector3::Zero)
+		m_app(app), m_pos(spawnPos), m_rot()
 	{
 		const float rad = std::atan2f(target.z - m_pos.z, target.x - m_pos.x);
 		m_vec = Vector3(std::cos(rad), 0, std::sin(rad)) * 0.1f;
@@ -32,7 +32,8 @@ public:
 		if (Random() > 0.99f) m_vec.x *= -1;
 		if (Random() > 0.99f) m_vec.z *= -1;
 
-		m_rot += Vector3(0, 0.1f, 0.1f);
+		m_rot *= Quaternion::CreateFromAxisAngle(Vector3::Right, m_vec.z * 0.5f);
+		m_rot *= Quaternion::CreateFromAxisAngle(Vector3::Forward, m_vec.x * 0.5f);
 	}
 
 	void draw() override
@@ -44,7 +45,8 @@ public:
 	Vector3 getPos() const { return m_pos; }
 private:
 	App* m_app;
-	Vector3 m_pos, m_rot, m_vec;
+	Vector3 m_pos, m_vec;
+	Quaternion m_rot;
 };
 
 using EnemyManagerPtr = std::shared_ptr<ActorManager<Enemy>>;
