458f6ab8029beba55c93029072e217145ea7c3c4
yashihei <yashihei@gmail.com>
2017-12-05 00:44:30 +0900

    Improve Singleton

--- Source/Singleton.h
+++ Source/Singleton.h
@@ -9,19 +9,15 @@
 template <class Type>
 class Singleton {
 public:
-	static Type* getInstance()
-	{
-		static Type* instance;
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
 
-		if (instance == nullptr) {
-			instance = new Type();
-		}
+	static Type& instance()
+	{
+		static Type instance;
 		return instance;
 	}
 protected:
 	Singleton() = default;
 	virtual ~Singleton() = default;
-private:
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
 };
