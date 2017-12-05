5f89ce641ce8bf24799afcab62acba64f2bf7268
yashihei <yashihei@gmail.com>
2017-12-05 16:47:55 +0900

    Support mouse click

--- Source/InputManager.h
+++ Source/InputManager.h
@@ -20,32 +20,32 @@ public:
 		m_gamePad = std::make_unique<DirectX::GamePad>();
 	}
 
-	bool isPressedButton1() const { return m_keyboard->GetState().Z || m_gamePad->GetState(m_padIndex).IsYPressed(); }
-	bool isPressedButton2() const { return m_keyboard->GetState().X || m_gamePad->GetState(m_padIndex).IsBPressed(); }
-	bool isPressedButton3() const { return m_keyboard->GetState().C || m_gamePad->GetState(m_padIndex).IsAPressed(); }
+	bool isPressedButton1() const { return m_keyboard->GetState().Z || m_gamePad->GetState(m_padIndex).IsYPressed() || m_mouse->GetState().leftButton; }
+	bool isPressedButton2() const { return m_keyboard->GetState().X || m_gamePad->GetState(m_padIndex).IsBPressed() || m_mouse->GetState().rightButton; }
+	bool isPressedButton3() const { return m_keyboard->GetState().C || m_gamePad->GetState(m_padIndex).IsAPressed() || m_mouse->GetState().middleButton; }
 	bool isPressedButton4() const { return m_keyboard->GetState().V || m_gamePad->GetState(m_padIndex).IsXPressed(); }
 	bool isPressedUp() const { return m_keyboard->GetState().Up || m_gamePad->GetState(m_padIndex).dpad.up; }
 	bool isPressedDown() const { return m_keyboard->GetState().Down || m_gamePad->GetState(m_padIndex).dpad.down; }
 	bool isPressedLeft() const { return m_keyboard->GetState().Left || m_gamePad->GetState(m_padIndex).dpad.left; }
 	bool isPressedRight() const { return m_keyboard->GetState().Right || m_gamePad->GetState(m_padIndex).dpad.right; }
 
-	bool isClicledButton1() const { return m_keyTracker.pressed.Z || m_padTracker.y == ButtonState::PRESSED; }
-	bool isClicledButton2() const { return m_keyTracker.pressed.X || m_padTracker.b == ButtonState::PRESSED; }
-	bool isClicledButton3() const { return m_keyTracker.pressed.C || m_padTracker.a == ButtonState::PRESSED; }
-	bool isClicledButton4() const { return m_keyTracker.pressed.V || m_padTracker.x == ButtonState::PRESSED; }
-	bool isClicledUp() const { return m_keyTracker.pressed.Up || m_padTracker.dpadUp == ButtonState::PRESSED; }
-	bool isClicledDown() const { return m_keyTracker.pressed.Down || m_padTracker.dpadDown == ButtonState::PRESSED; }
-	bool isClicledLeft() const { return m_keyTracker.pressed.Left || m_padTracker.dpadLeft == ButtonState::PRESSED; }
-	bool isClicledRight() const { return m_keyTracker.pressed.Right || m_padTracker.dpadRight == ButtonState::PRESSED; }
+	bool isClicledButton1() const { return m_keyTracker.pressed.Z || m_padTracker.y == PadButtonState::PRESSED || m_mouseTracker.leftButton == MouseButtonState::PRESSED; }
+	bool isClicledButton2() const { return m_keyTracker.pressed.X || m_padTracker.b == PadButtonState::PRESSED || m_mouseTracker.rightButton == MouseButtonState::PRESSED; }
+	bool isClicledButton3() const { return m_keyTracker.pressed.C || m_padTracker.a == PadButtonState::PRESSED || m_mouseTracker.middleButton == MouseButtonState::PRESSED; }
+	bool isClicledButton4() const { return m_keyTracker.pressed.V || m_padTracker.x == PadButtonState::PRESSED; }
+	bool isClicledUp() const { return m_keyTracker.pressed.Up || m_padTracker.dpadUp == PadButtonState::PRESSED; }
+	bool isClicledDown() const { return m_keyTracker.pressed.Down || m_padTracker.dpadDown == PadButtonState::PRESSED; }
+	bool isClicledLeft() const { return m_keyTracker.pressed.Left || m_padTracker.dpadLeft == PadButtonState::PRESSED; }
+	bool isClicledRight() const { return m_keyTracker.pressed.Right || m_padTracker.dpadRight == PadButtonState::PRESSED; }
 
-	bool isRelesedButton1() const { return m_keyTracker.released.Z || m_padTracker.y == ButtonState::RELEASED; }
-	bool isRelesedButton2() const { return m_keyTracker.released.X || m_padTracker.b == ButtonState::RELEASED; }
-	bool isRelesedButton3() const { return m_keyTracker.released.C || m_padTracker.a == ButtonState::RELEASED; }
-	bool isRelesedButton4() const { return m_keyTracker.released.V || m_padTracker.x == ButtonState::RELEASED; }
-	bool isRelesedUp() const { return m_keyTracker.released.Up || m_padTracker.dpadUp == ButtonState::RELEASED; }
-	bool isRelesedDown() const { return m_keyTracker.released.Down || m_padTracker.dpadDown == ButtonState::RELEASED; }
-	bool isRelesedLeft() const { return m_keyTracker.released.Left || m_padTracker.dpadLeft == ButtonState::RELEASED; }
-	bool isRelesedRight() const { return m_keyTracker.released.Right || m_padTracker.dpadRight == ButtonState::RELEASED; }
+	bool isRelesedButton1() const { return m_keyTracker.released.Z || m_padTracker.y == PadButtonState::RELEASED || m_mouseTracker.leftButton == MouseButtonState::RELEASED; }
+	bool isRelesedButton2() const { return m_keyTracker.released.X || m_padTracker.b == PadButtonState::RELEASED || m_mouseTracker.rightButton == MouseButtonState::RELEASED; }
+	bool isRelesedButton3() const { return m_keyTracker.released.C || m_padTracker.a == PadButtonState::RELEASED || m_mouseTracker.middleButton == MouseButtonState::RELEASED; }
+	bool isRelesedButton4() const { return m_keyTracker.released.V || m_padTracker.x == PadButtonState::RELEASED; }
+	bool isRelesedUp() const { return m_keyTracker.released.Up || m_padTracker.dpadUp == PadButtonState::RELEASED; }
+	bool isRelesedDown() const { return m_keyTracker.released.Down || m_padTracker.dpadDown == PadButtonState::RELEASED; }
+	bool isRelesedLeft() const { return m_keyTracker.released.Left || m_padTracker.dpadLeft == PadButtonState::RELEASED; }
+	bool isRelesedRight() const { return m_keyTracker.released.Right || m_padTracker.dpadRight == PadButtonState::RELEASED; }
 
 	bool isConnectedPad() const { return m_gamePad->GetState(m_padIndex).IsConnected(); }
 	void changePadIndex(const int index) { m_padIndex = index; }
@@ -74,6 +74,7 @@ private:
 	DirectX::Keyboard::KeyboardStateTracker m_keyTracker;
 	DirectX::Mouse::ButtonStateTracker m_mouseTracker;
 	DirectX::GamePad::ButtonStateTracker m_padTracker;
-	using ButtonState = DirectX::GamePad::ButtonStateTracker::ButtonState;
+	using PadButtonState = DirectX::GamePad::ButtonStateTracker::ButtonState;
+	using MouseButtonState = DirectX::Mouse::ButtonStateTracker::ButtonState;
 	int m_padIndex;
 };
