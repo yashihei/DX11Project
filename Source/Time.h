f027442e43a28dd58ce0727d2336140b24e51b3f
yashihei <yashihei@gmail.com>
2017-12-10 20:41:51 +0900

    [improve] Time class

--- Source/Time.h
+++ Source/Time.h
@@ -14,19 +14,22 @@ namespace hks {
 	
 class Time {
 public:
-	Time() : m_timeScale(1), m_prevTime(timeGetTime()), m_deltaTime(0) {}
+	Time() : m_timeScale(1), m_prevTime(timeGetTime()), m_deltaTime(0), m_totalTime(0) {}
 
 	void update()
 	{
-		m_deltaTime = timeGetTime() - m_prevTime;
+		m_deltaTime = (timeGetTime() - m_prevTime) * m_timeScale / 1000.f;
+		m_totalTime += m_deltaTime;
 		m_prevTime = timeGetTime();
 	}
 
 	void changeScale(float scale) { m_timeScale = scale; }
-	float deltaTime() const { return m_deltaTime / 1000.f * m_timeScale; }
+	float deltaTime() const { return m_deltaTime; }
+	float totalTime() const { return m_totalTime; }
 private:
 	float m_timeScale;
-	DWORD m_prevTime, m_deltaTime;
+	DWORD m_prevTime;
+	float m_deltaTime, m_totalTime;
 };
 
 } // namespace hks
