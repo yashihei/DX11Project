a66d6204e40912411abeaec880e1c42a2123e6a8
yashihei <yashihei@gmail.com>
2017-12-11 15:55:09 +0900

    [update] enemyの移動を時間に同期

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -10,29 +10,32 @@
 #include "Random.h"
 #include "AssetsManager.h"
 #include "UtilFunc.h"
+#include "Time.h"
 
 namespace sp4rk {
 
 Enemy::Enemy(App* app, Vector3 spawnPos, Vector3 target):
 	m_app(app), m_pos(spawnPos), m_rot()
 {
+	const float speed = 5.0f;
 	const float rad = std::atan2f(target.z - m_pos.z, target.x - m_pos.x);
-	m_vec = Vector3(std::cos(rad), 0, std::sin(rad)) * 0.1f;
+	m_vec = Vector3(std::cos(rad), 0, std::sin(rad)) * speed;
 }
 
 void Enemy::update()
 {
-	m_pos += m_vec;
+	const float deltaTime = m_app->getTime()->deltaTime();
+
+	m_pos += m_vec * deltaTime;
 	m_pos.x = Clamp(m_pos.x, -20.f, 20.f);
 	m_pos.z = Clamp(m_pos.z, -20.f, 20.f);
 
-	//change vec
-	//TODO:timer‚Å§Œä‚·‚é
+	// ˆê’èŠm—¦‚Å•ûŒü“]Š·
 	if (Random() > 0.99f) m_vec.x *= -1;
 	if (Random() > 0.99f) m_vec.z *= -1;
 
-	m_rot *= Quaternion::CreateFromAxisAngle(Vector3::Right, m_vec.z);
-	m_rot *= Quaternion::CreateFromAxisAngle(Vector3::Forward, m_vec.x);
+	m_rot *= Quaternion::CreateFromAxisAngle(Vector3::Right, m_vec.z * deltaTime);
+	m_rot *= Quaternion::CreateFromAxisAngle(Vector3::Forward, m_vec.x * deltaTime);
 }
 
 void Enemy::draw()
