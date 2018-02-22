01612c10fab15b31a0af842d7365d03144903bca
yashihei <yashihei@gmail.com>
2018-02-22 12:04:00 +0900

    [add] EffectBase

---
+++ Source/EffectBase.h
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------
+// File : EffectBase.h
+// Copyright (c) 2018 yashihei
+// Licensed under the MIT license
+// -----------------------------------------------
+
+#pragma once
+
+class EffectBase {
+public:
+	EffectBase();
+	virtual ~EffectBase();
+	virtual void apply() = 0;
+private:
+};
